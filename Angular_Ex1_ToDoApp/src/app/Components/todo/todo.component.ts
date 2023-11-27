f87eae4839858d9c528df18c6955cb55b1ddbf7c
TanimaM <119323593+TanimaM@users.noreply.github.com>
2023-11-27 15:56:07 +0530

    Add files via upload

---
+++ Angular_Ex1_ToDoApp/src/app/Components/todo/todo.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  public newTask: string = '';
+  public tasks: any[] = [];
+  public completedTasks: any[] = [];
+
+  public addTask(): void {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push({ description: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  public deleteTask(task: any): void {
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
+
+  public markAsCompleted(task: any):void {
+    task.completed = true;
+    this.completedTasks.push(task);
+    this.deleteTask(task);
+  }
+
+  public deleteCompletedTask(completedTask: any):void {
+    const index = this.completedTasks.indexOf(completedTask);
+    if (index !== -1) {
+      this.completedTasks.splice(index, 1);
+    }
+  }
+}
