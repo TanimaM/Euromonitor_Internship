c24fcba667545827e788ea786d38cd2facca74c0
TanimaM <119323593+TanimaM@users.noreply.github.com>
2023-12-29 01:14:46 +0530

    Update date-sort.pipe.ts

--- Task Management System/src/app/pipes/date-sort.pipe.ts
+++ Task Management System/src/app/pipes/date-sort.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateSort'
 })
 export class DateSortPipe implements PipeTransform {
-  transform(array: any[], field: string): any[] {
+  public transform(array: any[], field: string): any[] {
     return [...array].sort((a, b) => {
       const dateComparison = new Date(a[field]).getTime() - new Date(b[field]).getTime();
       return dateComparison !== 0 ? dateComparison : array.indexOf(a) - array.indexOf(b);
